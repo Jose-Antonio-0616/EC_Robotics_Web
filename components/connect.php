4ff463ee4a9ff50f676ddefb98ae3b84cc0f9a85
Jose-Antonio-0616 <137839867+Jose-Antonio-0616@users.noreply.github.com>
2024-01-03 23:37:18 -0400

    nuevos archivos

---
+++ components/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'shop_db';
+
+$conn = new PDO("mysql: host=$servername;dbname=$database", $username, $password);
+
+?>
+
+<!-- nota -->
