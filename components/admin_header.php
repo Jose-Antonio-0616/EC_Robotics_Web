0ed57a4defc85fb3c323945bdacf34f07e4ad25c
Jose-Antonio-0616 <137839867+Jose-Antonio-0616@users.noreply.github.com>
2024-01-24 21:10:06 -0400

    Add files via upload

--- components/admin_header.php
+++ components/admin_header.php
@@ -1,29 +1,29 @@
 <?php
-   if(isset($message)){
-      foreach($message as $message){
-         echo '
+if (isset($message)) {
+   foreach ($message as $message) {
+      echo '
          <div class="message">
-            <span>'.$message.'</span>
+            <span>' . $message . '</span>
             <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
          </div>
          ';
-      }
    }
+}
 ?>
 
 <header class="header">
 
    <section class="flex">
 
-      <a href="../admin/dashboard.php" class="logo">Admin<span>Panel</span></a>
+      <a href="../admin/dashboard.php" class="logo">Panel<br><span>Administración</span></a>
 
       <nav class="navbar">
-         <a href="../admin/dashboard.php">home</a>
-         <a href="../admin/products.php">products</a>
-         <a href="../admin/placed_orders.php">orders</a>
+         <a href="../admin/dashboard.php">inicio</a>
+         <a href="../admin/products.php">productos</a>
+         <a href="../admin/placed_orders.php">pedidos</a>
          <a href="../admin/admin_accounts.php">admins</a>
-         <a href="../admin/users_accounts.php">users</a>
-         <a href="../admin/messages.php">messages</a>
+         <a href="../admin/users_accounts.php">usuarios</a>
+         <a href="../admin/messages.php">mensajes</a>
       </nav>
 
       <div class="icons">
@@ -33,17 +33,17 @@
 
       <div class="profile">
          <?php
-            $select_profile = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
-            $select_profile->execute([$admin_id]);
-            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         $select_profile = $conn->prepare("SELECT * FROM `administradores` WHERE id = ?");
+         $select_profile->execute([$admin_id]);
+         $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
          ?>
-         <p><?= $fetch_profile['name']; ?></p>
-         <a href="../admin/update_profile.php" class="btn">update profile</a>
+         <p><?= $fetch_profile['nombre']; ?></p>
+         <a href="../admin/update_profile.php" class="btn">actualizar perfil</a>
          <div class="flex-btn">
-            <a href="../admin/register_admin.php" class="option-btn">register</a>
-            <a href="../admin/admin_login.php" class="option-btn">login</a>
+            <a href="../admin/register_admin.php" class="option-btn">registrar</a>
+            <a href="../admin/admin_login.php" class="option-btn">ingresar</a>
          </div>
-         <a href="../components/admin_logout.php" class="delete-btn" onclick="return confirm('logout from the website?');">logout</a> 
+         <a href="../components/admin_logout.php" class="delete-btn" onclick="return confirm('cerrar sesión?');">cerrar sesión</a>
       </div>
 
    </section>
